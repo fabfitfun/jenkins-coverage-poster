840d47a5af381470ae4c3ccc75d7a74256fd75cd
khurramcheema <kmc4ever@gmail.com>
2021-05-05 22:37:49 +0500

    - changed token to header

--- src/com/spotify/jenkinsfile/Coverage.groovy
+++ src/com/spotify/jenkinsfile/Coverage.groovy
@@ -88,7 +88,7 @@ def Double getCoverage(String ref) {
 
       COMMIT_STATUS_URL=\$(echo "https://\${GITHUB_API_URL}/repos/\${ORG}/\${REPO}/commits/\${COMMIT_HASH}/status")
       TOKEN_PARAM="access_token=\$TOKEN"
-      COMMIT_JSON=\$(curl "\${COMMIT_STATUS_URL}" -H "Authorization: \${TOKEN}")
+      COMMIT_JSON=\$(curl -H "Authorization: \${TOKEN}" "\${COMMIT_STATUS_URL}" )
 
       echo \$COMMIT_JSON | python -c 'import sys, json
 
@@ -122,7 +122,7 @@ def postCommitStatus(String state, String context, String description) {
       TOKEN_PARAM="access_token=\${TOKEN}"
       COMMIT_STATUS_URL=\$(echo "https://\${GITHUB_API_URL}/repos/\${ORG}/\${REPO}/statuses/\${COMMIT_HASH}")
 
-      curl -isSL -X POST "\${COMMIT_STATUS_URL}" -H "Authorization: \${TOKEN}" -d '{
+      curl -isSL -H "Authorization: \${TOKEN}"  -X POST "\${COMMIT_STATUS_URL}" -d '{
         \"state\": \"${state}\",
         \"target_url\": \"'\${BUILD_URL}'\",
         \"context\": \"${context}\",
